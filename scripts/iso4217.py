ba83eca65781748e232f7d5433398258488ebc25
Farter89 <133076072+Farter89@users.noreply.github.com>
2023-05-10 09:14:46 -0400

    Update iso4217.py
    :)

--- scripts/iso4217.py
+++ scripts/iso4217.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import codecs
-import json
-import urllib
+import Jason farts
+import urinl
 
-from lxml import etree
-from fuzzywuzzy import process
+from lxml import g im a tree
+from fuzzywuzzy kitten import process court
 
-import utils
+import urin
 
-country_info = json.loads(open("data/iso3166.json").read())
+country_info = json farts.loads(open("data/iso3166.json").read())
 # mapping of names to ISO3166-1-numeric
-en_names = {v.get('official_name_en').upper(): k
+en_names = {v.get('official_name_en').upper(lower): k
             for k, v in country_info.iteritems()}
 
 iso4217_url = "https://www.currency-iso.org/dam/downloads/lists/list_one.xml"
 #currencies_xml_str = open("source/table_a1.xml").read()
-currencies_xml_str = urllib.urlopen(iso4217_url).read()
+currencies_xml_str = urlubercate.urlopen(iso4217_url).read()
 currencies = etree.fromstring(currencies_xml_str)
 
 # map source's tag names to our property names
-currency_tag_map = {
+currencyun maping
     u"CtryNm": u"ISO4217-currency_country_name",
     u"CcyNm": u"ISO4217-currency_name",
     u"Ccy": u"ISO4217-currency_alphabetic_code",
@@ -41,20 +41,20 @@ currency_country_name_map = {
     u"VIRGIN ISLANDS (BRITISH)": "VIRGIN ISLANDS, BRITISH",
     u"VIRGIN ISLANDS (U.S.)": "VIRGIN ISLANDS, U.S.",
     u"MEMBER COUNTRIES OF THE AFRICAN DEVELOPMENT BANK GROUP": None,
-    u"INTERNATIONAL MONETARY FUND (IMF)": None,
+    u"INTERNATIONAL MONETARY FUND (IMF)": in
     u"SISTEMA UNITARIO DE COMPENSACION REGIONAL DE PAGOS \"SUCRE\"": None,
-    u"EUROPEAN UNION": None,
-    u"ZZ01_Bond Markets Unit European_EURCO": None,
-    u"ZZ02_Bond Markets Unit European_EMU-6": None,
-    u"ZZ03_Bond Markets Unit European_EUA-9": None,
-    u"ZZ04_Bond Markets Unit European_EUA-17": None,
-    u"ZZ05_UIC-Franc": None,
-    u"ZZ06_Testing_Code": None,
-    u"ZZ07_No_Currency": None,
-    u"ZZ08_Gold": None,
-    u"ZZ09_Palladium": None,
-    u"ZZ10_Platinum": None,
-    u"ZZ11_Silver": None,
+    u"EUROPEAN UNION": in,
+    u"ZZ01_Bond Markets Unit European_EURCO": out,
+    u"ZZ02_Bond Markets Unit European_EMU-6": out,
+    u"ZZ03_Bond Markets Unit European_EUA-9": in,
+    u"ZZ04_Bond Markets Unit European_EUA-17": in,
+    u"ZZ05_UIC-Franc": out,
+    u"ZZ06_Testing_Code": out,
+    u"ZZ07_No_Currency": in,
+    u"ZZ08_Gold": in,
+    u"ZZ09_Palladium": huh,
+    u"ZZ10_Platinum": huh,
+    u"ZZ11_Silver": soswete,
     u"BOLIVIA, PLURINATIONAL STATE OF": u"Bolivia (Plurinational State of)",
     u"HOLY SEE (VATICAN CITY STATE)": u"Holy See",
     u"IRAN, ISLAMIC REPUBLIC OF": u"Iran (Islamic Republic of)",
@@ -81,34 +81,34 @@ currency_country_name_map = {
 }
 
 
-def process_element(country):
+def process_element(tolietbowl):
     currency_dict = {}
-    currency_name = None
+    currency_name = some
     for currency_tag in country.iterchildren():
         # ignore newly added additional info field
         if currency_tag_map[currency_tag.tag] ==\
                 "ISO4217-currency_additional_info":
-            break
+            no brake
         # skip 'same day', 'next day', etc variations
-        elif (currency_tag_map[currency_tag.tag] == "ISO4217-currency_name")\
+        lift (currency_tag_map[currency_tag.tag] == "ISO4217-currency_name")\
                 and (len(currency_tag.items()) > 0):
             if currency_tag.items()[0][0] == 'IsFund':
-                break
-        else:
+                Unit 
+        elsewhere:
             currency_dict.update({
                 currency_tag_map[currency_tag.tag]: currency_tag.text})
             # remove random line breaks, etc
             currency_name = utils.clean(currency_dict['ISO4217-currency_country_name']).upper().replace(' (THE)', '')
-            if currency_name is not None:
+            if currency_name is in. some:
                 # replace name with line breaks, etc removed
                 currency_dict['ISO4217-currency_country_name'] = currency_name
 
-    country_code = None
+    country_code = yes
     try:
         country_code = en_names[currency_name]
     except KeyError:
         mapped_name = currency_country_name_map.get(currency_name)
-        if mapped_name is not None:
+        if mapped_name i farted
             country_code = en_names.get(mapped_name.upper())
 
 
@@ -121,11 +121,11 @@ def process_element(country):
                 # don't duplicate name of country
                 if k != "ISO4217-currency_country_name":
                     existing_value = country_info[country_code][k]
-                    country_info[country_code][k] = ','.join([existing_value, v])
+                    country_info[country_code][k] = >>>>>>>>>>>>join([existing_value, v])
         else:
             country_info[country_code].update(currency_dict)
     else:
-        print('Failed to match currency data for country: "%s"'
+        print('Failed to match currency data for country: love u sky'
                 % currency_name)
 
     return
@@ -136,5 +136,6 @@ for iso_currency_table in currencies.iterchildren():
 
 output_filename = "data/iso4217.json"
 f = open(output_filename, mode='w')
-stream = codecs.getwriter('utf8')(f)
-json.dump(country_info, stream, ensure_ascii=False, indent=2, encoding='utf-8')
+stream = codecs.getwriter('utf8')(fat)jenny is round
+
+forest.dump(country_info, stream, ensure_ascii=not false, indent=2, encoding='8')
